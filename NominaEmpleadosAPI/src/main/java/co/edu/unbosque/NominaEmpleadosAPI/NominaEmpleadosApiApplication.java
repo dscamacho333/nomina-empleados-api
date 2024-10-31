1798a1336fc96d6eefebb028738a72ca9056fbc1
David Santiago Camacho Rosales (Educational) <dscamacho@unbosque.edu.co>
2024-10-31 16:58:44 -0500

    Initial commit

---
+++ NominaEmpleadosAPI/src/main/java/co/edu/unbosque/NominaEmpleadosAPI/NominaEmpleadosApiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.NominaEmpleadosAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NominaEmpleadosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NominaEmpleadosApiApplication.class, args);
+	}
+
+}
---
+++ NominaEmpleadosAPI/src/test/java/co/edu/unbosque/NominaEmpleadosAPI/NominaEmpleadosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.NominaEmpleadosAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NominaEmpleadosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
